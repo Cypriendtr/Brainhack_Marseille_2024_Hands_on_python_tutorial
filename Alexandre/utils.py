192dcc174ea7cc298a6d1361be3b2cb641bde2e5
alexandre-laine <118631107+alexandre-laine@users.noreply.github.com>
2024-11-13 17:28:49 +0100

    Pandas part (the exercice is not here for the moment)

--- Alexandre/utils.py
+++ Alexandre/utils.py
@@ -1,12 +1,19 @@
-from tqdm import tqdm
+import tqdm
 import time
 import os
 
 cwd = os.getcwd()
 
-def progress_bar(min):
-    
-    duration = min*60
-    
-    for i in tqdm(range(duration)):
+def func_timer(duration:float):
+    """
+    Parameters
+    ---------
+    duration : Duration in minutes
+    Returns
+    ---------
+    Progress bar of the duration referenced
+    """
+
+    dur_sec = duration * 60
+    for i in tqdm.trange(dur_sec, ncols=90, desc="Exercise timer", ascii="_|"):
         time.sleep(1)
